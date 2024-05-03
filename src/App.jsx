533efd341f0d1328d269e0c51b052887d51b369b
rahulsubagio <rahulsubagio99@gmail.com>
2024-05-03 17:37:36 +0700

    buat halaman homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import './components/LogoIoT'
+import LogoIoT from './components/LogoIoT'
+
 export default function App() {
   return (
-    <>
-      <main>
-        <h1 className="text-3xl text-center">Hello Guys</h1>
-      </main>
-    </>
+    <main>
+      <div className='card'>
+        <LogoIoT/>
+        <h4 className='text-center font-semibold my-5'>
+          Implementasi Adaptive Fuzzy Logic Controller pada Tanaman Hidroponik Melon
+        </h4>
+        <button>
+          <h3>Login</h3>
+        </button>
+      </div>
+    </main>
   )
 }
---
+++ src/components/LogoIoT.jsx
@@ -0,0 +1,7 @@
+export default function LogoIoT() {
+  return (
+    <div>
+      <img src="/public/iot-logo.png" alt="iot logo" className="iot-logo"/>
+    </div>
+  )
+}
